dc19ab64c4fe31f917fc0c56ad9bcea79e4f0dab
Noel Sebu <noelsebu555@gmail.com>
2025-09-02 17:43:31 +0530

    dbconfig

---
+++ dbconfig/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+interface ConnectionStates {
+  isConnected?: number;
+}
+
+const connectionStates: ConnectionStates = {};
+
+export async function connect(): Promise<void> {
+  if (connectionStates.isConnected) {
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI as string);
+
+    connectionStates.isConnected = db.connection.readyState;
+    console.log("✅ Database connected:", db.connection.host);
+  } catch (error) {
+    console.error("❌ Error in database connection\n", error);
+    process.exit(1);
+  }
+}
